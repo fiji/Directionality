ecd1621facbac083bcdb76f26d37c5f585ffcb6f
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2018-03-14 15:17:28 +0100

    Release v2.2.0.
    - Fix incorrect getNBins method. Fixed by @giocard.

--- src/main/java/fiji/analyze/directionality/Directionality_.java
+++ src/main/java/fiji/analyze/directionality/Directionality_.java
@@ -260,7 +260,7 @@ public class Directionality_ implements PlugIn
 
 	private static final String PLUGIN_NAME = "Directionality analysis";
 
-	private static final String VERSION_STR = "2.0.1";
+	private static final String VERSION_STR = "2.2.0";
 
 	/* USER SETTING FIELDS, memorized between runs */
 	private static boolean setting_debug = false;
